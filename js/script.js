fa674cfab8669bf32e0f6d65641905e11d4a7e21
MykhayloOdesa <m.yervachov@gmail.com>
2022-11-03 20:40:08 +0200

    initial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let userInput = prompt("Яка офіційна назва JavaScript?");
+// console.log(userInput);
+
+// if (userInput === "ECMAScript") {
+//   alert("Вірно!");
+// } else {
+//   alert('Не знаєте? ECMAScript!');
+// }
+
+// const message =
+//   userInput === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!";
+
+// alert(message);
