cd03dd97cdcd46902f922118307683d01a684345
KobzarAnatolii <TohaPirat@gmail.com>
2022-11-03 20:52:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// Використовуючи функцію if...else,
+//напишіть код, який запитуватиме:
+//"Яка офіційна назва JavaScript?"
+//Якщо користувач вводить "ECMAScript",
+// показати через alert: "Вірно!"
+//інакше відобразити:"Не знаєте? ECMAScript!"
+
+
 // let userInput = prompt("Яка офіційна назва JavaScript?");
 // console.log(userInput);
 
@@ -11,3 +19,24 @@
 //   userInput === "ECMAScript" ? "Вірно!" : "Не знаєте? ECMAScript!";
 
 // alert(message);
+
+
+// task 2
+
+// Напишіть цикл, який виводить у консоль
+// числа від max до min за спаданням
+// Додайте (сумма) усі парні числа від max до min
+
+// const max = 50;
+// const min = 23;
+// let total = 0;
+
+// for (let i = max; i >= min; i -= 1){
+    
+//     if (i % 2 !== 0) {
+//        continue
+//     }
+//     total += i
+// }
+
+// console.log(total);
