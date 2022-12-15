21c48ba37126edeb63e09d2e14083b471d9403f2
KobzarAnatolii <TohaPirat@gmail.com>
2022-12-15 22:26:21 +0200

    task31

--- js/script.js
+++ js/script.js
@@ -591,45 +591,51 @@
 //змінюватися на іншу
 // - Форма щоразу повинна з'являтися у різних місцях на сторінці
 // - Колір форми в рандомному порядку змінюється,
-function getRangomColor() {
-  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-}
-
-function getRandomHex() {
-  return Math.round(Math.random() * 256)
-    .toString(16)
-    .padStart(2, '0');
-}
-
-
-const forms = [
-  'width: 100px; height: 100px; border-width: 1px; border-color: #000000',
-  'width: 100px; height: 100px; border-radius: 50%; border-width: 1px; border-color: #000000',
-  'width: 150px; height: 100px; border-width: 1px; border-color: #000000',
-  'width: 200px; height: 100px; border-radius: 100px / 50px;',
-  'width: 150px; height: 100px; transform: skew(20deg);',
-];
-const randomither = max => {
-  return Math.floor(Math.random() * max);
-};
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRangomColor() {
+//   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
 // }
-const form = document.createElement('div');
-document.body.append(form);
-onFormClick();
-form.addEventListener('click', onFormClick);
-function onFormClick() {
-  form.style.cssText = forms[randomither(forms.length)];
-form.style.backgroundColor = getRangomColor();
-form.style.position = "absolute";
-let height = 100 - form.clientHeight*100/document.documentElement.clientHeight;
-form.style.top = `${randomither(height)}%`;
-let width = 100 - form.clientWidth*100/document.documentElement.clientWidth;
-form.style.left = `${randomither(width)}%`;
+
+// function getRandomHex() {
+//   return Math.round(Math.random() * 256)
+//     .toString(16)
+//     .padStart(2, '0');
+// }
+
+// const forms = [
+//   'width: 100px; height: 100px; border-width: 1px; border-color: #000000',
+//   'width: 100px; height: 100px; border-radius: 50%; border-width: 1px; border-color: #000000',
+//   'width: 150px; height: 100px; border-width: 1px; border-color: #000000',
+//   'width: 200px; height: 100px; border-radius: 100px / 50px;',
+//   'width: 150px; height: 100px; transform: skew(20deg);',
+// ];
+// const randomither = max => {
+//   return Math.floor(Math.random() * max);
+// };
+
+// // function getRandomHexColor() {
+// //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// // }
+// const form = document.createElement('div');
+// document.body.append(form);
+// onFormClick();
+// form.addEventListener('click', onFormClick);
+// function onFormClick() {
+//   form.style.cssText = forms[randomither(forms.length)];
+// form.style.backgroundColor = getRangomColor();
+// form.style.position = "absolute";
+// let height = 100 - form.clientHeight*100/document.documentElement.clientHeight;
+// form.style.top = `${randomither(height)}%`;
+// let width = 100 - form.clientWidth*100/document.documentElement.clientWidth;
+// form.style.left = `${randomither(width)}%`;
+// }
+
+const listEl = document.querySelector('.js-accordion-list');
+console.log(listEl);
+listEl.addEventListener('click', onBtnClick);
+
+function onBtnClick(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+  event.target.nextElementSibling.classList.toggle('active');
 }
-
-
-
-
